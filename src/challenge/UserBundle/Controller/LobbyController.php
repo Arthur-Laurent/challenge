9eda310190b26108456aef711f6d16327bec6efc
raxer971 <laurentarthurcontact@gmail.com>
2015-10-25 00:21:06 +0200

    Gestions admin

--- src/challenge/UserBundle/Controller/LobbyController.php
+++ src/challenge/UserBundle/Controller/LobbyController.php
@@ -2,6 +2,7 @@
 
 namespace challenge\UserBundle\Controller;
 
+use challenge\PaymentBundle\Entity\Orders;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class LobbyController extends Controller
@@ -24,5 +25,16 @@ class LobbyController extends Controller
             )
 
         );
+    }
+    public function finishAction(Orders $orders)
+    {
+        $em=$this->getDoctrine()->getManager();
+        $orders->setCompleted(true);
+        $em->flush();
+        $lobby=$this->getDoctrine()->getManager()->getRepository('challengePaymentBundle:Orders')->getLobby();
+
+        return $this->render('challengeUserBundle:Lobby:index.html.twig', array(
+            'orders'=>$lobby
+        ));
     }
 }
